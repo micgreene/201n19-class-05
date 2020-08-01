4e96fc047ff6a65b13dd0c7982b300db2834cbcc
mikegreene <mgreene11111@yahoo.com>
2020-08-01 15:44:33 -0700

    finished problem 3

--- starter-code/app.js
+++ starter-code/app.js
@@ -50,7 +50,7 @@ IMPORTANT DETAIL: You may not use the arithmetic operators + and * in this funct
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumAndMultiply() function and see if the test passes.*/
 
 // Write your code here
-function sumAndMultiply(a, b, c) { //eslint-disable-line
+function sumAndMultiply (a, b, c) { //eslint-disable-line
 
 }
 
